06b1782e5a6ff585def4ea80c223b5e2ffc4f842
yuanmengqing <18518303942@163.com>
2018-01-22 08:54:08 +0800

    consumer  HelloController

---
+++ consumerHello/src/main/java/consumer/hello/api/HelloController.java
@@ -0,0 +1,20 @@
+package consumer.hello.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    @RequestMapping(value = "hello",method = RequestMethod.GET)
+    public String hello(){
+        return restTemplate.getForEntity("http://SERVICE-HELLO/hello",String.class).getBody();
+    }
+}
